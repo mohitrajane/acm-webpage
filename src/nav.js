1e2c143ae7c31ef970a5b0f5660e9da84fa08a5a
mohitrajane <mhit98@gmail.com>
2020-05-01 22:42:45 +0530

    alt navbar used

--- src/nav.js
+++ src/nav.js
@@ -1,86 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.png';
 import {Link} from 'react-router-dom';
-import Menu from './menu-24px.svg';
 
 
-
-class Nav extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            isOpen:false
-        };
-        this.showContent = this.showContent.bind(this);
-    }
-    showContent(){
-        this.setState({isOpen: !this.state.isOpen});
-        console.log(this.state.isOpen);
-    }
-    render(){
-        return(
-            <div>
-                {/*This is the code for the navbar for the smaller devices */}
-                <div class="smalldevice sm:block md:hidden lg:hidden bg-gray-100 text-blue-600" id="myTopnav">
-                    <div class="flex flex-wrap">
-                        <Link to="/">
-                            <img class="h-16 w-16 mx-4 mt-4" src={logo}/>
-                        </Link> 
-                        <button onClick={this.showContent}>
-                            <img src={Menu}/>
-                        </button>
-                    </div>
-                    {this.state.isOpen?
-                        <div class="dropdownelements grid-cols-1" id="Content">
-                            <Link>
-                                <a>Events</a>
-                            </Link><br/>
-                            <Link>
-                                <a>Gallery</a>
-                            </Link><br/>
-                            <Link to="/people">
-                                <a>People</a>
-                            </Link><br/>
-                            <Link>
-                                <a>Contact</a>
-                            </Link><br/>
-
-                            <Link>
-                                <a>Events</a>
-                            </Link><br/>
-                        </div>
-                    :''
-                    }
-                </div>
-                {/*This is the code for the navbar for the larger devices */}
-                <div class="topnavbar hidden md:block lg:block" style={{position:"fixed",top:"0px",width:'100%'}}> 
-                    <nav class="flex bg-gray-100 h-24 justify-center">
-                        {/*this is for the larger devices */}
-                        <div class="largedevices text-blue-600 flex">
-                            <Link class="mt-8" to="/event">
-                            <a class="px-2 mx-4">Events</a>   
-                            </Link>
-                            <a class="px-2 mx-4 mt-8" href="https://www.acm.org/membership/join">Membership</a>   
-                            <Link class="mt-8">
-                            <a class="px-2 mx-4">Gallery</a>  
-                            </Link>
-                            <Link to="/">
-                            <img class="h-16 w-16 mx-4 mt-4" src={logo}/>
-                            </Link> 
-                            <Link class="mt-8" to="/People">
-                            <a class="px-2 mx-4">People</a>
-                            </Link>
-                            <Link class="mt-8">
-                            <a class="px-2 mx-4">Contact</a>
-                            </Link>   
-                            <Link class="mt-8">
-                            <a class="px-2 mx-4">News</a>
-                            </Link>
-                        </div>
-                    </nav>
-                </div>
+const Navbar = () =>{
+    const [isOpen, setIsOpen] = useState(window.innerWidth <= 640 ? false : true);
+    return(
+        <nav className="flex items-center justify-between flex-wrap bg-gray-100 text-blue-600 p-2">
+            <div className="flex items-center flex-shrink-0 mr-6">
+                <img class="h-16 w-16 mx-4 mt-4" src={logo}/>
             </div>
-        );
-    }
-}
-export default Nav;
+            <div className="block lg:hidden">
+                <button className="flex items-center px-3 py-2 border rounded text-blue-200 border-blue-400 hover:text-blue-600 hover:border-grey-100">
+                <svg onClick={()=>setIsOpen(!isOpen)} className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </button>
+            </div>
+            {isOpen?
+                <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                    <div className="text-sm lg:flex-grow">
+                    <Link to="event" className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-blue-800">
+                        Events
+                    </Link>
+                    <a href="https://www.acm.org/membership/join" className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-blue-800">
+                        Membership
+                    </a>
+                    <Link to="gallery" className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-blue-800">
+                        gallery
+                    </Link>
+                    <Link to="/People" className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-blue-800">
+                        People
+                    </Link>
+                    <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-blue-800">
+                        Contact
+                    </Link>
+                    <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-blue-800">
+                        News
+                    </Link>
+                   
+                    </div>
+                 </div>
+                : ''
+            }
+        </nav>
+    );
+};
+
+export default Navbar;
