9e49b28d25d45e48f44f7bd5aa7559e36e953c45
Naveen B Jacob <naveenbjacob123@gmail.com>
2020-05-11 20:01:33 +0530

    the head content

--- src/component/Heads.js
+++ src/component/Heads.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 
-import Naveen from './images/naveen.jpg';
+
 import headDetails from '../res/heads.json';
 import {AiOutlineArrowLeft,AiOutlineArrowRight} from 'react-icons/ai';
 
@@ -9,32 +9,52 @@ export default function Head(props){
     for(var i=0; i<years.length;i++) years[i] = +years[i];
     var date = parseInt(new Date(Date.now()).toLocaleString().split(',')[0].split('/')[2]);
     const [startYear,setcurrentYear] = useState(date-1);
-    console.log(startYear);
-    
-    function decrement(e)
-    {
-        return(years.includes(e-1)?e=e-1:console.log("hello"));
-    }
-    function increment(e)
-    {
-        return(years.includes(e+1)?e=e+1:console.log("hello"));
-    }
-    // let a=()=>{
-    //     years.includes(startYear-1)?startYear=startYear-1:console.log("hello");
-    // }
-    return(
-        <div className="text-center ">
-            <p>2018</p>
-            <div className="flex">
-                <button className="bg-gray-100" onClick={decrement}>
-                    <AiOutlineArrowLeft/></button>
-                <div id="2018">
-                    <div className="flex w-56">
-                        <img src={Naveen}/>
 
-                    </div>
-                </div>
-                <button className="bg-gray-100" onClick={increment}><AiOutlineArrowRight/></button>
+    
+    function decrement()
+    {
+        if(years.includes(startYear-1)){
+            setcurrentYear(startYear-1);
+            document.getElementById('decrementer').style.visibility='visible';
+            document.getElementById('incrementer').style.visibility='visible';
+
+        }
+        else{
+            document.getElementById('decrementer').style.visibility='hidden';
+        }
+        console.log(startYear);
+    }
+    function increment()
+    {
+        if(years.includes(startYear+1)){
+            setcurrentYear(startYear+1);
+            document.getElementById('decrementer').style.visibility='visible';
+            document.getElementById('incrementer').style.visibility='visible';
+
+        }
+        else{
+            document.getElementById('incrementer').style.visibility='hidden';
+        }
+        console.log(startYear);
+    }
+    // years.map((item)=>{
+    //     console.log(headDetails[item]['post']);
+    // })
+
+    return(
+        <div className="text-center p-1">
+            <p>{startYear}</p>
+            <div className="flex justify-center">
+                <button className="bg-gray-100" id="decrementer" onClick={decrement}><AiOutlineArrowLeft/></button>
+                    {headDetails[startYear].map(events=>(
+                        <div>
+                            <img src={events.image} alt="heads"/>
+                            <p>{events.post}</p>
+                        </div>
+                    ))}
+
+
+                <button className="bg-gray-100" id="incrementer" onClick={increment}><AiOutlineArrowRight/></button>
             </div>
         </div>
     );
