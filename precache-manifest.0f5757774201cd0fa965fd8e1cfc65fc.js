c205ad118c1754d04ea40a0c13b5abb62d2c9ca6
Mohit Rajan <mohit@impress.ai>
2020-06-09 21:52:13 +0530

    Updates

---
+++ precache-manifest.0f5757774201cd0fa965fd8e1cfc65fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444c866dc593c7e5eb8efaaf72b4b380",
+    "url": "/acm-webpage/index.html"
+  },
+  {
+    "revision": "434995ff2dea1275ce25",
+    "url": "/acm-webpage/static/css/main.5cf02fa1.chunk.css"
+  },
+  {
+    "revision": "2337d0633bf94fe200e8",
+    "url": "/acm-webpage/static/js/2.15f198d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/acm-webpage/static/js/2.15f198d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434995ff2dea1275ce25",
+    "url": "/acm-webpage/static/js/main.e400d2f4.chunk.js"
+  },
+  {
+    "revision": "9fce32d2770541c4b296",
+    "url": "/acm-webpage/static/js/runtime-main.f3c1e8d0.js"
+  },
+  {
+    "revision": "e9888737a856bc9b6b5f509bff2d5e59",
+    "url": "/acm-webpage/static/media/Acmlogo.e9888737.jpeg"
+  },
+  {
+    "revision": "6d7fe3620eb0abb205f360ec33f59e8c",
+    "url": "/acm-webpage/static/media/logo.6d7fe362.png"
+  },
+  {
+    "revision": "4306a5b8990fbd3f94632d58aba1060c",
+    "url": "/acm-webpage/static/media/man.4306a5b8.svg"
+  },
+  {
+    "revision": "f3b7d629c80a85788fe1cc7377576fec",
+    "url": "/acm-webpage/static/media/phone.f3b7d629.svg"
+  }
+]);
