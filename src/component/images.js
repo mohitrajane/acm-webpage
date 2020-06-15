e6b572658817ed9664c3c824cdcfb85aa2f81e2b
Naveen B Jacob <naveenbjacob123@gmail.com>
2020-06-15 23:14:00 +0530

    hosting it

--- src/component/images.js
+++ src/component/images.js
@@ -1,19 +1,13 @@
-import React from "react";
-// import Image from "../res/images.json";
-
-
-export default function Images(){
+import React from 'react';
+import Image from '../res/images.json';
+export default function react(){
     return(
         <div>
-            {/* {    
-            Object.keys(Image).reverse().map(function(key){
-                Image[key].map(item=>(
-                    <div className='p-2' key={item.key}>
-                    <img className="w-24" src={item.image} alt="heads"/>
-                    <p>{item.post}</p>
-                </div>
-                ))
-            })} */}
+            {
+            Object.keys(Image).forEach(function(key){
+                console.log(key,Image[key])
+            })
+            }
         </div>
     );
 }
