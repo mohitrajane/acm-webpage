6f48dbc4d779317326b3cbf2cf0376a3d90bfb49
Naveen B Jacob <naveenbjacob123@gmail.com>
2020-06-15 23:38:39 +0530

    hosting

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import Images from "./component/images";
 export default function App(){
     const [isOpen, setIsOpen] = useState(window.innerWidth <= 640 ? false : true);
   return(
-    <Router>
+    <Router basename={"/acm-webpage"}>
               <nav className="flex items-center justify-between flex-wrap bg-gray-100 text-blue-600 p-2 lg:fixed lg:w-full lg:top-0 ">
-            <Link to="/">
+            <Link to={`${process.env.PUBLIC_URL}/`}>
                 <div className="flex items-center flex-shrink-0 mr-6">
                     <img className="h-16 w-16 mx-4 mt-4" src={logo} alt="logo"/>
                 </div>
@@ -25,16 +25,14 @@ export default function App(){
             {isOpen?
                 <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                     <div className="text-sm lg:flex-grow">
-                    <Link to="/event" className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-blue-800">
+                    <Link to={`${process.env.PUBLIC_URL}/event`} className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-blue-800">
                         Events
                     </Link>
-                    <Link to="/images" className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-blue-800">
-                        Images
-                    </Link>
+
                     <a href="https://docs.google.com/forms/d/e/1FAIpQLScNwhsLm_jtOpGKV4UxWSUsE2v1Q7fL_MK3cAzJTYBa71jLAQ/viewform?usp=sf_link" className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-blue-800">
                         Membership
                     </a>
-                    <Link to="/people" className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-blue-800">
+                    <Link to={`${process.env.PUBLIC_URL}/people`} className="block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-blue-800">
                         People
                     </Link>
                     </div>
@@ -45,18 +43,16 @@ export default function App(){
         <div>
 
           <div className="mt-24">
-            <Route exact path="/">
+            <Route exact path={`${process.env.PUBLIC_URL}/`}>
               <MainPage/>
             </Route>
-            <Route path="/event">
+            <Route path={`${process.env.PUBLIC_URL}/event`}>
               <Events/>
             </Route>
-            <Route path="/people">
+            <Route path={`${process.env.PUBLIC_URL}/people`}>
               <People/>
             </Route>
-            <Route>
-              <Images/>
-            </Route>
+
           </div>
 
         </div>
