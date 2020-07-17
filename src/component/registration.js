cb46f334a7ef689682b3cd5116ee52d919b5a16f
Naveen B Jacob <naveenbjacob123@gmail.com>
2020-07-17 19:42:47 +0530

    moved the registration form to a IFrame and displayed it

---
+++ src/component/registration.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Iframe from 'react-iframe';
+
+export default function Membership(){
+    return(
+        <div>
+            <Iframe
+            className="w-full h-screen"
+            url="https://docs.google.com/forms/d/e/1FAIpQLScNwhsLm_jtOpGKV4UxWSUsE2v1Q7fL_MK3cAzJTYBa71jLAQ/viewform?usp=sf_link"
+            position="relative"
+            
+            
+            />
+        </div>
+    );
+}
